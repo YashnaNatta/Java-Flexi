0e10a9dbf535809cb5b181f32ab98ef3719cd756
Natta Yashna <yashnanatta@gmail.com>
2026-01-19 02:25:58 +0530

    Added java prgrm

---
+++ Details.java
@@ -0,0 +1,7 @@
+class Details {
+    public static void main(String[] args) {
+        System.out.println("Name:Yashna");
+        System.out.println("Roll No:117");
+        System.out.println("Address:pune");
+    }
+}

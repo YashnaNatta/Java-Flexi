e981cd4a21425fd182bc8a87b1b010d84013c4ad
Natta Yashna <yashnanatta@gmail.com>
2026-01-19 03:05:59 +0530

    MethodOverloading

---
+++ MethodOverloading.java
@@ -0,0 +1,14 @@
+class MethodOverloading {
+    void add(int a,int b){
+        System.out.println("Sum: "+(a+b));
+    }
+    void add(double a,double b){
+        System.out.println("Sum: "+(a+b));
+
+    }
+    public static void main(String[] arsg){
+        MethodOverloading obj = new MethodOverloading();
+    obj.add(10,20);
+    obj.add(5.5,4.5);
+    }
+}
